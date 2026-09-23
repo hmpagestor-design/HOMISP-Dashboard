a49230822aa6a2c23a90bdbef90deaa50f605830
João Victor Fernandes Cabral <31513227+developerjoao@users.noreply.github.com>
2026-09-23 11:22:32 -0300

    Substitui JSONP por CORS no dashboard público

--- dashboard-config.js
+++ dashboard-config.js
@@ -1,2 +1,2 @@
 window.HOMISP_PUBLIC_API_URL = "https://script.google.com/macros/s/AKfycbyQ20Icf9T6I4MQU8dbMoa3YWl0ZD4BpDofV49tr972WuCHb_dyGog67jJ5HeLZGQ38/exec";
-window.HOMISP_PUBLIC_API_MODE = "jsonp";
+window.HOMISP_PUBLIC_API_MODE = "cors";
